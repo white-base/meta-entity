5ef9964604e81d5c28a62ba528db4e09e98980b2
logicfeel <logicfeel@gmail.com>
2024-03-19 07:00:42 +0900

    타입로딩 테스트

---
+++ temp/타입로딩.js
@@ -0,0 +1,22 @@
+
+
+
+var Util                        = require('logic-core').Util;
+var Observer                    = require('logic-core').Observer;
+var Message                     = require('logic-core').Message;
+
+
+var MetaElement                 = require('logic-core').MetaElement;
+var MetaTable                   = require('../src/meta-table').MetaTable;
+
+
+var iii = new MetaTable('ff')
+
+// var ii = new BaseEntity()   // 나부
+
+var i2 = new Observer({})
+var i3 = new MetaElement('d')
+var i4 = Message.get()
+
+
+console.log(0);
